9d87f8e7bb15ad8dc27a6322aa45989d5f81a8e7
Shaheen <67522759+webtooweb@users.noreply.github.com>
2024-03-10 20:12:09 -0600

    message

--- js/controllers/deleteToDoController.js
+++ js/controllers/deleteToDoController.js
@@ -1,29 +1,57 @@
-import { deleteToDo } from '../models/toDoListModel'
+// import { deleteToDo } from '../models/toDoListModel'
 
-let dialog
-let deleteButton
-let exitButton
-let uid
+// let dialog
+// let deleteButton
+// let exitButton
+// let uid
+
+// export function deleteToDoController(itemUid) {
+//     uid = itemUid
+//     dialog = document.querySelector('#delete-to-do')
+//     exitButton = dialog.querySelector('#exit')
+//     deleteButton = dialog.querySelector('#delete')
+//     configureListeners()
+//     dialog.showModal()
+// }
+
+// function configureListeners() {
+//     exitButton.addEventListener('click', onCloseDialog)
+//     deleteButton.addEventListener('click', onRemoveToDo)
+// }
+
+// function onCloseDialog(e) {
+//     dialog.close()
+// }
+
+// function onRemoveToDo(e) {
+//     deleteToDo(uid); 
+//     onCloseDialog(); 
+// }
+import { deleteToDo } from '../models/toDoListModel';
+
+let dialog;
+let uid;
 
 export function deleteToDoController(itemUid) {
-    uid = itemUid
-    dialog = document.querySelector('#delete-to-do')
-    exitButton = dialog.querySelector('#exit')
-    deleteButton = dialog.querySelector('#delete')
-    configureListeners()
-    dialog.showModal()
+    uid = itemUid;
+    dialog = document.querySelector('#delete-to-do');
+    configureListeners();
+    dialog.showModal();
 }
 
 function configureListeners() {
-    exitButton.addEventListener('click', onCloseDialog)
-    deleteButton.addEventListener('click', onRemoveToDo)
+    const exitButton = dialog.querySelector('#exit');
+    const deleteButton = dialog.querySelector('#delete');
+
+    exitButton.addEventListener('click', onCloseDialog);
+    deleteButton.addEventListener('click', onRemoveToDo);
 }
 
 function onCloseDialog(e) {
-    dialog.close()
+    dialog.close();
 }
 
 function onRemoveToDo(e) {
     deleteToDo(uid); 
-    onCloseDialog(); 
+    onCloseDialog(e); 
 }
